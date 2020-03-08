3cd88d34bd273eec91f0391f6af68885d27eee5b
vahidbk <v.babaee@gmail.com>
2020-03-08 10:59:39 +0330

    اصلاح روش کلید کردن بر روی کلید خرید و فروش و یافتن چند نوع پیام مرتبط با خرید و فروش بدون پییده سازی

--- Codes/Gold.py
+++
@@ -1,9 +0,0 @@
-from codal.Groups import updateGroups
-from gold.Gold import updateGoldData
-import json
-import time
-
-print(json.dumps(updateGoldData(), indent = 4))
-
-time.sleep(10)
-
--- Codes/mofidBroker/easyTraderWidget.py
+++ Codes/mofidBroker/easyTraderWidget.py
@@ -3,8 +3,33 @@ from PyQt5.QtWidgets import QDialog, QApplication
 from mofidBroker.easytraderdialog import Ui_Dialog
 from mofidBroker.EasyTraderScraper import *
 
+         
 class AppWindow(QDialog):
     
+    def computeGold(self):
+        import threading
+        from codal.Groups import updateGroups
+        from gold.Gold import updateGoldData
+        import json
+        import time
+        
+        def goldComputeWorker():
+
+            buttonList=[self.ui.pushButton_15, self.ui.pushButton_16, \
+                self.ui.pushButton_17,self.ui.pushButton_18, \
+                self.ui.pushButton_19, self.ui.pushButton_20, \
+                self.ui.pushButton_21, self.ui.pushButton_22, \
+                self.ui.pushButton_23,self.ui.pushButton_24]
+            goldData=updateGoldData()
+            buttonListCounter=0
+            for key in goldData:
+                buttonList[buttonListCounter].setText( key+":"+str(goldData[key]) )
+                buttonListCounter+=1
+            return
+
+        t = threading.Thread(target=goldComputeWorker).start()
+        
+       
     def __init__(self):
         super().__init__()
         self.ui = Ui_Dialog()
@@ -20,30 +45,17 @@ class AppWindow(QDialog):
         self.ui.testResponceButton.clicked.connect(self.easyTraderScraper.testResponce)
         self.ui.closeChromeButton.clicked.connect(self.easyTraderScraper.closeChrome)
         
-        self.ui.openSearchMenuButton.clicked.connect(self.easyTraderScraper.openSearchMenu)
+        self.ui.computeGoldButton.clicked.connect(self.computeGold)
         self.ui.closeSearchMenuButton.clicked.connect(self.easyTraderScraper.closeSearchMenu)
         self.ui.focusOnSymbolButton.clicked.connect(self.easyTraderScraper.focusOnSymbol)
         
+        self.ui.startBuySymbolButton.clicked.connect(self.easyTraderScraper.startBuySymbol)
+        self.ui.startSellSymbolButton.clicked.connect(self.easyTraderScraper.startSellSymbol)
+
+
+        self.ui.pushButton_15.clicked.connect(self.computeGold)
+        
         self.show() 
         
-        import threading
-        def goldComputeWorker():
-            from codal.Groups import updateGroups
-            from gold.Gold import updateGoldData
-            import json
-            import time
-            buttonList=[self.ui.pushButton_15, self.ui.pushButton_16, \
-                self.ui.pushButton_17,self.ui.pushButton_18, \
-                self.ui.pushButton_19, self.ui.pushButton_20, \
-                self.ui.pushButton_21, self.ui.pushButton_22, \
-                self.ui.pushButton_23,self.ui.pushButton_24]
-            goldData=updateGoldData()
-            buttonListCounter=0
-            for key in goldData:
-                buttonList[buttonListCounter].setText( key+":"+str(goldData[key]) )
-                buttonListCounter+=1
-                
-            return
-
-        t = threading.Thread(target=goldComputeWorker).start()
+        
 
